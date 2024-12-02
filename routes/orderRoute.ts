23fb714be4f9b607105dba65ef60c5a3e4571e7d
fredholmisak <fredholmisak@outlook.com>
2024-12-02 22:05:38 +0100

    add order default values

--- routes/orderRoute.ts
+++ routes/orderRoute.ts
@@ -21,6 +21,28 @@ function orderRoute(): Hono<Env> {
     const orderService = new OrderService(c);
     const data = await c.req.json();
 
+
+    /*
+      {
+        "id": "order0000000001",
+        "customerId": "customer_1",
+        "orderDate": "2023-09-15",
+        "status": "processing",
+        "items": ["product00000001", "product00000002"],
+        "totalAmount": 149.98,
+        "deliveryDate": "2023-09-20"
+      },
+    */
+
+    if (!data.id) data.id = "order0000000099"
+    if (!data.customerId) data.customerId = "customer01"
+    if (!data.orderDate) data.orderDate = "2024-12-03"
+    if (!data.status) data.status = "processing"
+    if (!data.items) data.items = ["product00000001"]
+    if (!data.totalAmount) data.totalAmount = 149.98
+    if (!data.deliveryDate) data.deliveryDate = "2024-12-07"
+    
+
     // Validate required fields
     if (!data.customerId || !data.items || !Array.isArray(data.items)) {
       return c.json({ error: 'Customer ID and items are required.' }, 400);
